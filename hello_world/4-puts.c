2529576d45d768caf099a22c1b7fce87e8efe835
Arlind3553 <georgetesco@yahoo.com>
2023-06-16 02:49:00 -0700

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *Programming = "\"Programming is like building a multilingual puzzle";
+
+	puts(Programming);
+
+	return (0);
+
+}
