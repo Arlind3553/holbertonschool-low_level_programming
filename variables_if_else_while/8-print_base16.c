1121d0f3f282a15d26564f2eaab27b5fd1890aee
Arlind3553 <georgetesco@yahoo.com>
2023-06-19 02:56:25 -0700

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+putchar(i);
+int n;
+for (n = 97; n < 103; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
