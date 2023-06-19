00df4caf7ca40f26742d2aa56b7622d5398dd64e
Arlind3553 <georgetesco@yahoo.com>
2023-06-19 03:23:31 -0700

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,11 +7,15 @@
 int main(void)
 {
 int i;
-for (i = 48; i < 57; i++){
+for (i = 48; i < 57; i++)
+{
 putchar(i);
+if (i < 57)
+{
 putchar(',');
 putchar(' ');
 }
-putchar(i);
+}
+putchar('\n');
 return (0);
 }
