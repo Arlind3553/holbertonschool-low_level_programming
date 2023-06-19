210b81ed2eacb02d889f80763d423bbb0b782104
Arlind3553 <georgetesco@yahoo.com>
2023-06-19 07:06:42 -0700

    task 12

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 48; i < 58; i++)
-{
-for (j = 48; j < 58 ; j++)
-{
-if ((i != j) && (i < j))
-{
-putchar(i);
-putchar(j);
-if (i != 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58 ; j++)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char c;
-for (c = 'a'; c <= 'z'; ++c)
-{
-putchar(c);
-}
-printf("\n");
-return (0);
-}
+ /**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+ int main(void)
+ {
+     int i, j;
+     for (i=0; i<=98; i++){
+         for (j = i + 1; j <= 99; j++)
+         {
+             putchar(i);
+             putchar(j);
+         }
+         
+     }
+    return (0);
+ }
