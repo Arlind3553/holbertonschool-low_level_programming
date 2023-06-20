0f9b6bf3624a3340356cbeebb813d9edffc99368
Arlind3553 <georgetesco@yahoo.com>
2023-06-20 04:05:18 -0700

    task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,22 +1,16 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
+ * _isalpha - it takes an argument c of value integer
  * @c: character
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if the character
+ * is a letter and returns 0 if not
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else 
-	{
-		return (0);
-	}
+if (c >= 97 && c <= 122)
+return (1);
+else if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
 }
-
