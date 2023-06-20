c79bbd6dbc30986cf7d1950c48186eef7af27ec8
Arlind3553 <georgetesco@yahoo.com>
2023-06-20 04:13:26 -0700

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - it takes an argument c of value integer
+ * @c: character
+ * Return: 1 if the character
+ * is a letter and returns 0 if not
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+	else if (n == 0)
+{
+	_putchar('0')
+	return (0);
+}
+	else
+	return (-1);
+}
