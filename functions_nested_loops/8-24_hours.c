05e8a69ac0f1094ea41c73155defd5dfffaa039c
Arlind3553 <georgetesco@yahoo.com>
2023-06-20 05:41:30 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i <= 23; i++)
+{
+for (j = i + 1; j <= 59; j++)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+}
+}
+}
