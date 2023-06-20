b4eccc4af1ea313594bde926611af6611eea5da8
Arlind3553 <georgetesco@yahoo.com>
2023-06-20 06:35:40 -0700

    task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - adds two numbers together
- * @a: first input
- * @b: second input
+ * @i: first input
+ * @j: second input
  * Return: returns the sum of the numbers
  */
 int add(int i, int j)
