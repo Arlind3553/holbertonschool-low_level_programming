fa2d439e1a2bd3c4c2c3f4f4d8564c534ea9780b
Arlind3553 <georgetesco@yahoo.com>
2023-06-20 03:38:16 -0700

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all alhpabet
+ * letters in lowercase 10 times
+ */
+int _islower(int c)
+{
+	if (c >= 41 && c <= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
