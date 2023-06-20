7a55e4fa62d0bc9b25771714a8c6ba5da167958d
Arlind3553 <georgetesco@yahoo.com>
2023-06-20 02:02:00 -0700

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int main(void)
 {
 char c;
@@ -11,6 +17,6 @@ for (c = 'a'; c <= 'z'; ++c)
 {
 	_putchar(c);
 }
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
