917f7c52848b902fbf949b5a6b95a70c6d3bb6e8
Arlind3553 <135588828+Arlind3553@users.noreply.github.com>
2023-06-21 17:28:21 +0200

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-int n;
-int i;
-for (i = 48; i < 58; i++)
-putchar(i);
-for (n = 97; n < 103; n++)
-putchar(n);
-putchar('\n');
+  int n;
+  int i;
+  for (i = 48; i < 58; i++)
+    putchar(i);
+  for (n = 97; n < 103; n++)
+  putchar(n);
+  putchar('\n');
 return (0);
 }
