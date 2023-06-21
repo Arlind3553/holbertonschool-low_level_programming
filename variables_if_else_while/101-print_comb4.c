7db154b9015b9fd4862aa8965dc978bcf0322495
Arlind3553 <135588828+Arlind3553@users.noreply.github.com>
2023-06-21 17:25:45 +0200

    Update 101-print_comb4.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-int i, j, k;
-for (i = 48; i < 58; i++)
-{
-for (j = 48; j < 58 ; j++)
-{
-for (k = 48; k < 58; k++)
-{
-if ((i != j) && (i < j) && (j < k) && (i != k) && (j != k))
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != 55)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
+  int i, j, k;
+  for (i = 48; i < 58; i++)
+  {
+    for (j = 48; j < 58 ; j++)
+    {
+      for (k = 48; k < 58; k++)
+      {
+        if ((i != j) && (i < j) && (j < k) && (i != k) && (j != k))
+        {
+          putchar(i);
+          putchar(j);
+          putchar(k);
+            if (i != 55)
+            {
+            putchar(',');
+            putchar(' ');
+            }
+          }
+        }
+      }
+    }
 putchar('\n');
 return (0);
 }
