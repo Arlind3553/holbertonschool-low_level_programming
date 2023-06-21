aea4de77cbe3788fd4addd66703456f656000f6e
Arlind3553 <135588828+Arlind3553@users.noreply.github.com>
2023-06-21 17:26:49 +0200

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; ++c)
-{
-putchar(c);
-}
-for (c = 'A'; c <= 'Z'; ++c)
-{
-putchar(c);
-}
-printf("\n");
+  char c;
+  for (c = 'a'; c <= 'z'; ++c)
+  {
+    putchar(c);
+  }
+  for (c = 'A'; c <= 'Z'; ++c)
+  {
+    putchar(c);
+  }
+  printf("\n");
 return (0);
 }
