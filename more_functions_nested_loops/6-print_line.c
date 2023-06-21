3d2f726090422099aa14b78d1cb82a510648f325
Arlind3553 <georgetesco@yahoo.com>
2023-06-21 12:05:08 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * more_numbers - main function
+ * Return: 1 if uppercase and 0 if false
+ */
+void print_line(int n)
+{
+	int i = 0;
+	while (i <= n)
+	{
+		_putchar('_');
+			i++;
+	}
+	_putchar('\n')
+	return (0)
+}
