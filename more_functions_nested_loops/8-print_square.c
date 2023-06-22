7b41bf41257d8b4e9ea021bfeaa11f1837e42705
Arlind3553 <georgetesco@yahoo.com>
2023-06-22 02:18:02 -0700

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_diagonal - main function
+ * @n: parameter
+ * Return: 1 if uppercase and 0 if false
+ */
+void print_square(int size)
+{
+	int 
+}
