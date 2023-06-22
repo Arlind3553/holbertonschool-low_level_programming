8d713c3c86ebfde5d1bdaa45114b1ab6e0da7d5e
Arlind3553 <georgetesco@yahoo.com>
2023-06-22 03:15:38 -0700

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,18 +2,24 @@
 #include "main.h"
 
 /**
- * print_diagonal - main function
- * @n: parameter
+ * print_square - main function
+ * @size: parameter
  * Return: 1 if uppercase and 0 if false
  */
+
 void print_square(int size)
 {
 	int i, j;
-	
-	for (i <= 0; i < size; i++)
+
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
-		_putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
