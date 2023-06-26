5c8828ce3dd1af4bce4706be57c739023f5a106a
Arlind3553 <georgetesco@yahoo.com>
2023-06-25 17:53:54 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - main function
+ * @a: first param
+ * @b: second param
+ */
+void swap_int(int *a, int *b)
+{
+	int random;
+
+	random = *a;
+	*a = *b;
+	*b = tmp;
+}
