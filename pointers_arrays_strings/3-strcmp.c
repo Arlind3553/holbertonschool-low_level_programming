de25a5aaccc50e69a45b1ed8902f386de58620ac
Arlind3553 <georgetesco@yahoo.com>
2023-06-27 03:19:21 -0700

    task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+
+}
