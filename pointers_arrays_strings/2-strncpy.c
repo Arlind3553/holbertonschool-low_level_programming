5be10e7bf2a8ff113a4b563cc23c193f43e00f6b
Arlind3553 <georgetesco@yahoo.com>
2023-06-27 01:52:57 -0700

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strncat - main function
+ * @dest: param
+ * @src: param
+ * @n: param
+ * Return: dest
+ */
+char *_strncpy(char *dest, const char *src, size_t n)
+{
+    size_t i;
+
+   for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    for ( ; i < n; i++)
+        dest[i] = '\0';
+
+   return dest;
+}
