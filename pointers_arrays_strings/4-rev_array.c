a6a43bb4ca584a11317f72c370497ffb3103ef38
Arlind3553 <georgetesco@yahoo.com>
2023-06-27 04:23:50 -0700

    task 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ *reverse_array -  function that reverses the content of an array of integers
+ *@a: pointer to array
+ *@n: n to be reversed
+*/
 void reverse_array(int *a, int n)
 {
 	int i = 0;
