e5c759a838e7d437bb0c1236add0ed0459a0173c
Arlind3553 <georgetesco@yahoo.com>
2023-06-27 03:56:04 -0700

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *_strncmp -  function that copies a string
+ *@s1: first string to be compared
+ *@s2: string to be compared with s1
+ *Return: returns an integer indicating the result of the
+ *comparison
+*/
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s1 == *s2)
