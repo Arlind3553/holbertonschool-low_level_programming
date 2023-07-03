a342120821d6d6093cf71d27130458bf1748518f
Arlind3553 <georgetesco@yahoo.com>
2023-07-03 03:06:13 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _puts_recursion - function that prints a string, followed by a new line.
- * @s: string
+ *_print_rev_recursion - function that prints a string in reverse
+ *@s: input
+ *Return: return 0
  */
-
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_putchar(s[i]);
-
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 	}
 }
