2d35c54115bc82637edbb5ea69858c53d093b8b3
Arlind3553 <georgetesco@yahoo.com>
2023-07-05 03:14:02 -0700

    task 1

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[])
+/**
+ * main - Entry point
+ * @argc: number of argumenst supplied to argv
+ * @argv: vectorial array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
 
 	(void) argc;
