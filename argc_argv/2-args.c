f715359d836737d912d3f9d6fc5021e16a227787
Arlind3553 <georgetesco@yahoo.com>
2023-07-05 03:23:52 -0700

    task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of argumenst supplied to argv
+ * @argv: vectorial array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	
+	int i;
+	(void) argc;
+
+	for(i = 0; i < argc; i++)
+		printf("%d\n", argv[i]);
+
+	return (0);
+}
