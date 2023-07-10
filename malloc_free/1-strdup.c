141dc1670a1432ee71259dc4adcc47577d8f4349
Arlind3553 <georgetesco@yahoo.com>
2023-07-10 02:43:33 -0700

    s

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+#include <string.h>
+/**
+ * _strdup - copys a given string and duplicates it
+ *@str: string to be duoplicated
+ *Return: pointer to the string duplicated
+ */
+char *_strdup(char *str)
+{
+	size_t len = strlen(str) + 1;
+	char *new = malloc(len);
+
+	if (new != NULL)
+	{
+		memcpy(new, str, len);
+	}
+	return (new);
+}
