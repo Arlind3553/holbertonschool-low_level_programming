038efce72d1b6f7156fd348066b8420d02602fc5
Arlind3553 <georgetesco@yahoo.com>
2023-07-10 03:26:47 -0700

    s

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@ char *str_concat(char *s1, char *s2)
 	size_t len;
 	char *new;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len = strlen(s1) + strlen(s2);
 	new = malloc(len + 1);
 	if (new == NULL)
