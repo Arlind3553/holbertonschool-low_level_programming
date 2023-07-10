baa51cd6dcd10158e1e454c704d01542c2e93c80
Arlind3553 <georgetesco@yahoo.com>
2023-07-10 03:17:27 -0700

    s

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new;
+	unsigned int len1, len2, i;
+	len1 = 0;
+	len2 = 0;
+	i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 = NULL)
+	       s2 = "";
+
+}
