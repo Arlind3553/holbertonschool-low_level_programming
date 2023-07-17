6c72c16ccdf3fc60cc64d5c2f7a8186bda0ea345
Arlind3553 <georgetesco@yahoo.com>
2023-07-17 05:01:37 -0700

    s

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog1 = malloc(sizeof(dog_t));
+
+	if(dog1 == NULL)
+		return (NULL);
+	dog1->name = malloc(strlen(name));
+	dog1->name = malloc(strlen(name));
+	strcpy(dog1->name, name);
+	strcpy(dog1->owner, owner);
+	newdog->age = age;
+	return (newdog);
+}
