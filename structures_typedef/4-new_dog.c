ccc723332f53d5ee5c9e9f148c623de95e01bf0a
Arlind3553 <georgetesco@yahoo.com>
2023-07-17 05:23:43 -0700

    s

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,7 +9,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if(dog1 == NULL)
 		return (NULL);
 	dog1->name = malloc(strlen(name) + 1);
-	dog1->name = malloc(strlen(name) + 1);
+	dog1->owner = malloc(strlen(name) + 1);
 	if (!dog1->name || !dog1->owner)
 	{
 		free(dog1->name);
