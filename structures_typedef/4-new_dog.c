9730524d6d7609634bf3457f524bffcc6a2986f5
Arlind3553 <georgetesco@yahoo.com>
2023-07-17 14:00:19 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,18 +5,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1 = malloc(sizeof(dog_t));
-
-	dog1->name = malloc(strlen(name) + 1);
-	dog1->owner = malloc(strlen(name) + 1);
+	if (dog1 == NULL)
+		return (NULL);
+	char *dogname = malloc(strlen(name) + 1);
+	char *dogowner = malloc(strlen(name) + 1);
 	if (dog1->name == NULL || dog1->owner == NULL || dog1 == NULL)
 	{
-		free(dog1->name);
-		free(dog1->owner);
+		free(dogname);
+		free(dogowner);
 		free(dog1);
 		return(NULL);
 	}
 	strcpy(dog1->name, name);
 	strcpy(dog1->owner, owner);
+	dog1->name = dogname;
+	dog1->owner = owner;
 	dog1->age = age;
 	return (dog1);
 }
