509d3d95e2f699e2ac76f3c7105fc6daf769e6fd
Arlind3553 <georgetesco@yahoo.com>
2023-07-19 02:37:51 -0700

    s

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -5,7 +5,7 @@
  *   *@n: number of parameters
  *    *@separator: string to be printed between numbers
  *     *Return: sum of all parameters
- *      */
+ **/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list va;
@@ -15,9 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(va, int));
-		if(separator)
+		if (separator)
 		{
-			if(i != n - 1)
+			if (i != n - 1)
 				printf("%s", separator);
 		}
 	}
