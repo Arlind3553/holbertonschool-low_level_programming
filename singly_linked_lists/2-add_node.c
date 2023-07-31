981d0dd5c33529f89d10bf36bd54bd7b6e5454c8
Arlind3553 <georgetesco@yahoo.com>
2023-07-31 03:52:01 -0700

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node)
+	if (new_node == NULL)
 	{
 		free(new_node->str);
 		free(new_node);
