174038aa345ad795597bcb2f3ff559841486d034
Arlind3553 <georgetesco@yahoo.com>
2023-07-31 07:37:32 -0700

    task 100

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__((constructor));
+
+void print_before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
