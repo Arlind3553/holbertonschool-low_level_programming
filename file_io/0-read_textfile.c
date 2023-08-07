e62bebe9386fe6c883c59b0158b085078a9ba4bc
Arlind3553 <georgetesco@yahoo.com>
2023-08-07 03:17:02 -0700

    task 1

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "main.h"
-
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: name of the file
+ * @letters: number of letters to copy
+ * Return: the number of letters it could read and print
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -20,8 +26,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	file_r = read(fd, txt, letters);
 	if (file_r == -1)
 		return (0);
-	file_w = write(STDOUT_FILENO, txt ,file_r);
-	if(file_w == -1)
+	file_w = write(STDOUT_FILENO, txt, file_r);
+	if (file_w == -1)
 		return (0);
 	close(fd);
 	return (file_r);
