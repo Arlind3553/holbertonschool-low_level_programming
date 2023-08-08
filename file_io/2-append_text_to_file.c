ecb571da078598c663171485c26e41de87910691
Arlind3553 <georgetesco@yahoo.com>
2023-08-08 02:03:35 -0700

    task 1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if(text_content != NULL)
+	if (text_content != NULL)
 	{
 		length = strlen(text_content);
 		n = write(fd, text_content, length);
