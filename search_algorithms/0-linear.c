f7438c681aa52041b280563d1189d3aeb6bc4fc1
Arlind3553 <georgetesco@yahoo.com>
2023-09-18 04:57:48 -0700

    task 1

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,16 +11,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if(array == NULL)
+	if (array == NULL || !value)
 		return (-1);
-
-	if (!value)
-		return (-1);
-
 	for (i = 0; i <= size; i++)
 	{
 		printf("Value checked array[%li] = [%d]\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 		{
 			return (i);
 		}
